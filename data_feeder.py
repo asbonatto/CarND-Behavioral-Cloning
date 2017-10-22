4f1d7e14e89abdb6490fbd984e264083b97a573a
André dos Santos Bonatto <andre.s.bonatto@gmail.com>
2017-10-22 10:00:32 -0200

    Added side cameras and image flipping. Also added validation generator

--- data_feeder.py
+++ data_feeder.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import cv2
 import sklearn
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 class DataFeeder():
     """
@@ -10,14 +12,23 @@ class DataFeeder():
     feed them to the network model
     """
     
-    def __init__(self, data_dir = "data", batch_size = 128):
+    def __init__(self, data_dir = "data", batch_size = 256, test_size = 0.20):
         
         self.logfile = os.path.join(data_dir, "driving_log.csv")
         self.batch_size = batch_size
         self.db_index = self._read_index()
-        self._list_campaigns()
-        self.nsamples = len(self.db_index)
+        self.db_index = self._list_campaigns()
+        self.add_side_cameras()
+        self.add_flipped()
+        
+        self.db_train, self.db_valid = train_test_split(self.db_index, test_size = test_size)
+        self.db_train.reset_index(inplace = True)
+        self.db_valid.reset_index(inplace = True)
+        
+        self.nsamples = len(self.db_train)
+        
         self.steps_per_epoch = self.nsamples // self.batch_size
+        self.validation_steps = len(self.db_valid) // self.batch_size
         
     def _read_index(self):
         """
@@ -61,30 +72,136 @@ class DataFeeder():
         df["campaign"] = df["campaign"].apply(lambda x: 1 if (x > 2) else 0)
         df["campaign"] = df["campaign"].cumsum()
         
-        self.db_index = df
+        return df
         
-    def fetch(self):
+    def describe_campaigns(self):
+        """
+        Density plots of the data acquisition campaigns
+        """
+        df = self.db_index
+
+        for campaign in df["campaign"].unique():
+            idx = df["campaign"] == campaign
+            plt.hist(df.loc[idx, "steering"])
+            plt.show()
+            
+    def add_side_cameras(self, correction = 0.20):
+        """
+        Extends the datasets with images from the side cameras
+        """
+        
+        correction_dict = {"right": -correction, "left": + correction}
+        
+        for side in ["right", "left"]:
+            df = self.db_index.copy()
+            df["center"] = df[side]
+            df["steering"]+= + correction_dict[side]
+            self.db_index = pd.concat([self.db_index, df], ignore_index = True)
+        
+    def add_flipped(self):
+        """
+        Doubles the dataset by marking a flipping algorithm in
+        """
+        self.db_index["flip"] = 0
+        df1 = self.db_index.copy()
+        df1["flip"] = 1
+        self.db_index = pd.concat([self.db_index, df1], ignore_index = True)
+        
+        
+    
+    def rebalance(self, nbins = 100):
+        """
+        Create bins of approximately the same size for sampling uniformly rebalancing the 
+        dataset
+        """
+        
+        df = self.db_index_raw
+        rebalanced = []
+        for campaign in df["campaign"].unique():
+            # First create the bins
+            idx = df["campaign"] == campaign
+            df.loc[idx, "bins"] = pd.qcut(df.loc[idx, "steering"], nbins, duplicates = "drop")
+            
+            # OLD : subsampling only the central bins
+            #bins = sorted(df.loc[idx].bins.unique())
+            #central_bins = [bin for bin in bins if bin.left*bin.right <= 0]
+            
+            groups = df.loc[idx].groupby(["bins"])
+            max_samples = int(np.percentile(groups.size(), 50))
+            
+            resample = groups.filter(lambda x: len(x) >= max_samples)
+            rebalanced.append(resample.apply(lambda x: x.sample(n= max_samples).reset_index(drop = True)))
+            
+            resample = groups.filter(lambda x: len(x) < max_samples)
+            rebalanced.append(resample.apply(lambda x: x.sample(frac= 1).reset_index(drop = True)))
+            
+            
+        self.db_index_raw = df    
+        self.db_index = pd.concat(rebalanced, ignore_index = True)
+        
+        
+        
+    def fetch_train(self):
         """
         Generator function to feed data to the model without
         exhausting the computer memory.
         OBS:
             Just small changes from the generator lesson
         """
-    
+        
         while 1: # Loop forever so the generator never terminates
-
             for offset in range(0, self.nsamples, self.batch_size):
-                batch_samples = self.db_index.loc[offset:offset + self.batch_size]
+                batch_samples = self.db_train.loc[offset:offset + (self.batch_size - 1)]
 
                 images = []
                 angles = []
                 for _, batch_sample in batch_samples.iterrows():
-                    center_image = cv2.imread("data/" + batch_sample["center"])
-                    center_angle = float(batch_sample["steering"])
+                    
+                    center_image, center_angle = get_data(batch_sample)
+                    
                     images.append(center_image)
                     angles.append(center_angle)
 
                 # trim image to only see section with road
                 X_train = np.array(images)
                 y_train = np.array(angles)
-                yield sklearn.utils.shuffle(X_train, y_train)    
+                yield sklearn.utils.shuffle(X_train, y_train)
+
+    def fetch_valid(self):
+        """
+        Generator function to feed data to the model without
+        exhausting the computer memory.
+        OBS:
+            Just small changes from the generator lesson
+        """
+        
+        while 1: # Loop forever so the generator never terminates
+            for offset in range(0, len(self.db_valid), self.batch_size):
+                batch_samples = self.db_valid.loc[offset:offset + (self.batch_size - 1)]
+
+                images = []
+                angles = []
+                for _, batch_sample in batch_samples.iterrows():
+                    
+                    center_image, center_angle = get_data(batch_sample)
+                    
+                    images.append(center_image)
+                    angles.append(center_angle)
+
+                # trim image to only see section with road
+                X_train = np.array(images)
+                y_train = np.array(angles)
+                yield sklearn.utils.shuffle(X_train, y_train)
+
+def get_data(batch_sample):
+    """
+    Helper function to flip the image
+    """
+    center_image = cv2.imread("data/" + batch_sample["center"])
+    center_angle = float(batch_sample["steering"])
+                    
+    if batch_sample["flip"] > 0:
+        center_image = cv2.flip(center_image, 1)
+        center_angle = center_angle*-1.0
+    
+    return center_image, center_angle
--- model.py
+++ model.py
@@ -6,19 +6,23 @@ Project : Behavioral Cloning
 
 import argparse
 from data_feeder import DataFeeder
+import matplotlib.pyplot as plt
 
 from keras.models import Sequential
 from keras.callbacks import ModelCheckpoint
 from keras.layers import Cropping2D, Lambda, BatchNormalization, Input
 from keras.layers.core import Dense, Activation, Flatten, Dropout
 from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.advanced_activations import LeakyReLU
+
 
 def enhanced_conv2d(model, filters, kernel_size, strides = (1,1)):
     """
     Custom layer that perform convolution followed by batch
     normalization.
     """
-    model.add(Conv2D(filters, kernel_size, strides=strides, padding='valid', activation = "elu"))
+    model.add(Conv2D(filters, kernel_size, strides=strides, padding='valid', activation = "linear"))
+    model.add(LeakyReLU(alpha = 0.01))
     model.add(BatchNormalization())
 
     return model
@@ -34,7 +38,7 @@ def nvidia_model():
     # Normalization layer
     model.add(Lambda(lambda x: x/255 - 0.5, input_shape = input_shape))
     # Cropping layer : target is 66x200x3
-    model.add(Cropping2D(cropping=((70, 24), (60, 60))))
+    model.add(Cropping2D(cropping=((74, 20), (60, 60))))
     # Start of the convolutional stack
     for filter in [24, 36, 48]:
         model = enhanced_conv2d(model, filter, 5, strides=(2, 2))
@@ -45,7 +49,10 @@ def nvidia_model():
     # The original model does not contain dropout layer
     model.add(Flatten())
     for neurons in [100, 50, 10]:
-        model.add(Dense(neurons, activation = 'elu'))
+        model.add(Dense(neurons, activation = 'linear'))
+        model.add(Dropout(0.5))
+        model.add(LeakyReLU(alpha = 0.01))
+        
  
     model.add(Dense(1))
     
@@ -56,19 +63,35 @@ def train_network(model, epochs = 5):
     Main script for training the Behavioral Cloning 
     Network model
     """
+    modelname = "model"
+    print(model.summary())
     
     data = DataFeeder()
     
     checkpoint = [ModelCheckpoint('model{epoch:02d}.h5')]
 
     model.compile(optimizer = "adam", loss = "mse")
-    model.fit_generator(data.fetch(), nb_epoch = epochs, verbose = 1, steps_per_epoch = data.steps_per_epoch, callbacks = checkpoint)
-    print(model.summary())
-    model.save("model.h5")
+    history = model.fit_generator(data.fetch_train(), nb_epoch = epochs, steps_per_epoch = data.steps_per_epoch, \
+                                  validation_data = data.fetch_valid(), validation_steps = data.validation_steps, \
+                                  verbose = 1, callbacks = checkpoint)
+    
+    model.save(modelname + ".h5")
+    print("Model saved to {}.h5".format(modelname))
+    
+    fig = plt.figure()
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['train', 'validation'], loc='upper right')
+    plt.savefig(modelname + '_training_history.png')
+    plt.close(fig)
     
     return "Finish"
 
 if __name__ == "__main__":
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("epochs", help = "# of epochs for training")
     args = parser.parse_args()
